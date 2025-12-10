f0fc91ac1705499497ea366e67f662db8a8a6a39
Sean Hennessey <SHennessey@DMV.nv.gov>
2025-12-10 10:12:24 -0800

    Work begun on importing bookmark html files.

---
+++ FFArchive/FFArchiveXXVI/UI/ImportBookmarkDialog.cs
@@ -0,0 +1,25 @@
+﻿namespace FFArchiveXXVI.UI;
+
+using System;
+using System.Windows.Forms;
+
+public partial class ImportBookmarkDialog : Form
+{
+    public ImportBookmarkDialog()
+    {
+        InitializeComponent();
+    }
+
+    private void ChooseImportFileButton_Click(object sender, EventArgs e)
+    {
+    }
+
+    private void OkButton_Click(object sender, EventArgs e)
+    {
+    }
+
+    private void CancelButton_Click(object sender, EventArgs e)
+    {
+        Close();
+    }
+}
